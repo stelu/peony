27cfcc1538874d9ae7631c5c96ad1a9d04329ac5
Steven Lu <slu@scrippsnetworks.com>
2013-11-13 09:51:23 -0500

    adding completedContactUsPage

---
+++ src/main/java/ulive/pages/UliveCompletedContactUSPage.java
@@ -0,0 +1,11 @@
+package main.java.ulive.pages;
+
+
+public class UliveCompletedContactUSPage {
+
+	//Contact US header links
+	public static final String COMPLETEDCONTACTUS_VIEWOURFAQ_LINK = "//div[@class='contact-success']/div/a";
+	public static final String COMPLETEDCONTACTUS_CONTACTUS_LINK = "//div[@class='contact-success']/div/a[2]";
+	
+	
+}
